cb7344f3daaa56ce2232311cf86d508abbe521fe
notchum <notchum@duck.com>
2025-04-02 19:42:49 -0600

    [ashen] add rice for ashen
    ashen themes on a sway system. included themes for:
    fzf
    eza
    yazi
    kitty

---
+++ rices/ashen/config/fzf/ashen.sh
@@ -0,0 +1,23 @@
+# Ashen Theme
+# Theme Author: Daniel Fichtinger
+# Ported By: eckertalex
+# License: MIT
+# Upstream: https://github.com/ficcdaf/ashen/blob/main/fzf/ashen.sh
+
+export FZF_DEFAULT_OPTS="
+  $FZF_DEFAULT_OPTS
+	--color=fg:#a7a7a7
+	--color=fg+:#d5d5d5
+	--color=bg:#121212
+	--color=bg+:#323232
+	--color=hl:#C4693D
+	--color=hl+:#E49A44
+	--color=info:#a7a7a7
+	--color=marker:#C4693D
+	--color=prompt:#C4693D
+	--color=spinner:#D87C4A
+	--color=pointer:#E5A72A
+	--color=header:#B14242
+	--color=border:#a7a7a7
+	--color=query:#d5d5d5
+	--color=gutter:#121212"
