3aa7ea2b8d68c334cb5950a61f552ee68d796bbc
notchum <notchum@duck.com>
2025-04-02 19:35:29 -0600

    add mapping to toggle list mode for nvim

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>tl", "<cmd>set list!<CR>", { desc = "toggle list mode"})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
