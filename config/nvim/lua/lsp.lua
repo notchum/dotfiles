72088a1dbfa5b55afd67bf518351679817c17a83
notchum <notchum@duck.com>
2025-08-26 02:00:24 -0600

    nvim add signature window

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -87,6 +87,42 @@ local function on_attach(client, bufnr)
     keymap('<BS>', '<C-o>s', {}, 's')
   end
 
+  -- signatures
+  -- Modified from https://github.com/NvChad/ui/blob/v3.0/lua/nvchad/lsp/signature.lua
+  vim.schedule(function()
+    local signatureProvider = client.server_capabilities.signatureHelpProvider
+    if signatureProvider and signatureProvider.triggerCharacters then
+      local function check_triggeredChars(triggerChars)
+        local cur_line = vim.api.nvim_get_current_line()
+        local pos = vim.api.nvim_win_get_cursor(0)[2]
+        local prev_char = cur_line:sub(pos - 1, pos - 1)
+        local cur_char = cur_line:sub(pos, pos)
+
+        for _, char in ipairs(triggerChars) do
+          if cur_char == char or prev_char == char then
+            return true
+          end
+        end
+      end
+
+      local group = vim.api.nvim_create_augroup("LspSignature", { clear = false })
+      vim.api.nvim_clear_autocmds { group = group, buffer = bufnr }
+
+      local triggerChars = client.server_capabilities.signatureHelpProvider.triggerCharacters
+
+      vim.api.nvim_create_autocmd("TextChangedI", {
+        group = group,
+        buffer = bufnr,
+        callback = function()
+          if check_triggeredChars(triggerChars) then
+            vim.lsp.buf.signature_help { focus = false, silent = true, max_height = 7, border = "single" }
+          end
+        end,
+      })
+    end
+  end)
+
+  -- general keymaps
   keymap('gD', vim.lsp.buf.declaration, { desc = "Go to declaration" }, 'n')
   keymap('gd', vim.lsp.buf.definition, { desc = "Go to definition" }, 'n')
   keymap('<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" }, 'n')
