50eb7547c94c7a00c35e2dea153b14a3a7601d52
notchum <notchum@duck.com>
2025-09-01 11:32:20 -0600

    link nvim colors with dotbot
    theme plugins are linked to `nvim/lua/colorscheme.lua`

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -33,7 +33,6 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
-  { src = "https://github.com/ficd0/ashen.nvim" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/stevearc/aerial.nvim" },
   { src = "https://github.com/kdheepak/lazygit.nvim" },
@@ -109,7 +108,7 @@ vim.diagnostic.config {
 }
 
 -- colors
-vim.cmd("colorscheme ashen")
+require("colorscheme")
 
 -- mappings
 local map = vim.keymap.set
