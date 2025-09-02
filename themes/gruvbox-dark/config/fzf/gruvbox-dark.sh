80472241a32a225e7231bb12a3a0e5deaa80c409
notchum <notchum@duck.com>
2025-09-02 12:03:42 -0600

    add more gruvbox app themes

---
+++ themes/gruvbox-dark/config/fzf/gruvbox-dark.sh
@@ -0,0 +1,17 @@
+export FZF_DEFAULT_OPTS="
+  $FZF_DEFAULT_OPTS
+	--color=fg:#ebdbb2
+	--color=fg+:#fbf1c7
+	--color=bg:#282828
+	--color=bg+:#32302f
+	--color=hl:#d79921
+	--color=hl+:#fabd2f
+	--color=info:#d65d0e
+	--color=marker:#fb4934
+	--color=prompt:#458588
+	--color=spinner:#fabd2f
+	--color=pointer:#fabd2f
+	--color=header:#d65d0e
+	--color=border:#bdae93
+	--color=query:#fbf1c7
+	--color=gutter:#282828"
