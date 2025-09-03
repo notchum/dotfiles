f3fc351dbd27848d571b0bb0c06052ef1ef37525
notchum <notchum@duck.com>
2025-09-03 11:46:00 -0600

    add configs for ayu-dark theme

---
+++ themes/ayu-dark/config/fzf/ayu-dark.sh
@@ -0,0 +1,18 @@
+export FZF_DEFAULT_OPTS="
+  $FZF_DEFAULT_OPTS
+	--color=fg:#BFBDB6
+	--color=fg+:#E6B450
+	--color=bg:#0B0E14
+	--color=bg+:#11151C
+	--color=hl:#FF8F40
+	--color=hl+:#FFB454
+	--color=info:#565B66
+	--color=marker:#F07178
+	--color=prompt:#FF8F40
+	--color=spinner:#D2A6FF
+	--color=pointer:#39BAE6
+	--color=header:#E6B673
+	--color=border:#636A72
+	--color=query:#BFBDB6
+	--color=gutter:#0B0E14"
+
